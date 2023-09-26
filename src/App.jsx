b236d99a8b38854629ffe534577363381a9436aa
swathimeenal <swathimeenal1994@gmail.com>
2023-09-26 09:45:51 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "./components/Sidebar"
+function App() {
+  
+
+  return (
+    <>
+      <div id="wrapper">
+        <Sidebar/>
+
+      </div>
+    </>
+  )
+}
+
+export default App
